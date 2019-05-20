a87918866955dad5315db0fd255da3e37c182e5c
Anjali walsatwar <anjali.walsatwar@huawei.com>
2019-05-20 17:26:44 +0530

    topology

---
+++ server.js
@@ -0,0 +1,24 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var fs = require("fs");
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 5002);
+app.use(express.logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
